d66184962cc3cdb0ec9d080a3c854071fbf72a5a
Michal Romanowski <michal.romanowski@staples-solutions.com>
2019-10-31 07:20:06 -0700

    kubernetes fix

--- kubectl-example.py
+++ kubectl-example.py
@@ -1,7 +1,12 @@
-from airflow import DAG
 from datetime import datetime, timedelta
+
+
+from airflow.models import DAG, DummyOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.contrib.kubernetes.secret import Secret
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.pod import Port
 
 
 default_args = {
@@ -16,32 +21,32 @@ default_args = {
 }
 
 dag = DAG(
-    'kubernetes_sample', default_args=default_  args, schedule_interval=timedelta(minutes=10))
+    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
 passing = KubernetesPodOperator(namespace='default',
-                          image="Python:3.6",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="passing-test",
-                          task_id="passing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
+                                image="Python:3.6",
+                                cmds=["Python", "-c"],
+                                arguments=["print('hello world')"],
+                                labels={"foo": "bar"},
+                                name="passing-test",
+                                task_id="passing-task",
+                                get_logs=True,
+                                dag=dag
+                                )
 
 failing = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:1604",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
+                                image="ubuntu:1604",
+                                cmds=["Python", "-c"],
+                                arguments=["print('hello world')"],
+                                labels={"foo": "bar"},
+                                name="fail",
+                                task_id="failing-task",
+                                get_logs=True,
+                                dag=dag
+                                )
 
 passing.set_upstream(start)
 failing.set_upstream(start)
