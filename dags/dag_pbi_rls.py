c4a92361fcaeed5e81275c4362491f8bb4b4c2c7
Michal Romanowski <michal.romanowski@staples-solutions.com>
2019-11-04 08:39:20 -0800

    introducing wh dags

--- dags/dag_pbi_rls.py
+++ dags/dag_pbi_rls.py
@@ -6,25 +6,22 @@ from sqlalchemy import create_engine
 from csbiETL.etl.warehouse.pbi_rls.wh_pbi_rls import wh_powerbi_rls
 from csbiETL import config
 
+
+schedule_interval = None
+dag_name = 'wh_pbi_rls'
+cdw_context = create_engine(config.CDW_CONNECTION_STRING)
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': airflow.utils.dates.days_ago(2),
 }
-
-cdw_context = create_engine(config.CDW_CONNECTION_STRING)
-
-dag = DAG(
-    'wh_pbi_rls', default_args=default_args, schedule_interval=None)
-
-
-wh_pbi = PythonOperator(
-    task_id='wh_powerbi_rls',
-    provide_context=False,
-    python_callable=wh_powerbi_rls,
-    op_kwargs={'db_context': cdw_context},
-    dag=dag,
-)
-
-if __name__ == "__main__":
-    dag.cli()
+# automatically assign new operators to that DAG
+# see: https://airflow.apache.org/concepts.html#context-manager
+with DAG(dag_name, default_args=default_args, schedule_interval=schedule_interval):
+    wh_pbi = PythonOperator(
+        task_id='wh_powerbi_rls',
+        provide_context=False,
+        python_callable=wh_powerbi_rls,
+        op_kwargs={'db_context': cdw_context}
+    )
