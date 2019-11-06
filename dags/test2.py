4e039f45da8769d04bbe1d5b055cb43051d8ffd0
Michal Romanowski <michal.romanowski@staples-solutions.com>
2019-11-06 01:21:04 -0800

    asd

---
+++ dags/test2.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+
+from csbiETL.common.DataLake import DataLake
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+schedule_interval = '0 /*5 * * *'
+dag_name = 'datalaketest'
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 11, 6, 10, 20)
+}
+
+def send():
+    with open('t.1', 'rb') as f:
+        DataLake(debug_mode=False).upload_file(f, 'test', 't.1')
+
+dag = DAG(dag_name, default_args=default_args, schedule_interval=schedule_interval)
+
+wh_pbi = PythonOperator(
+    task_id='send1',
+    provide_context=False,
+    python_callable=send,
+    dag=dag
+)
+
+
+
