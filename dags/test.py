6a1e92dfbc3c91b76f1d4d1d8a6c7f615c395175
Michal Romanowski <michal.romanowski@staples-solutions.com>
2019-11-06 01:00:07 -0800

    run

--- dags/test.py
+++ dags/test.py
@@ -29,26 +29,8 @@ run_this = BashOperator(
     bash_command='echo 1',
     dag=dag,
 )
-# [END howto_operator_bash]
 
 run_this >> run_this_last
-
-for i in range(3):
-    task = BashOperator(
-        task_id='runme_' + str(i),
-        bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        dag=dag,
-    )
-    task >> run_this
-
-# [START howto_operator_bash_template]
-also_run_this = BashOperator(
-    task_id='also_run_this',
-    bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    dag=dag,
-)
-# [END howto_operator_bash_template]
-also_run_this >> run_this_last
 
 if __name__ == "__main__":
     dag.cli()
